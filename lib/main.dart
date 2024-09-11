8c5baf84f141ef6b0b7e534d94f7c8bb0714fa4d
milan39 <97863179+Milan39@users.noreply.github.com>
2024-09-11 21:57:02 +0545

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'package:color_app/app_routes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-import 'app_routes.dart';
-
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
@@ -11,14 +9,9 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
